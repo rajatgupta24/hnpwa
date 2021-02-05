9aa67488707c94ad129ab9265719e77c798268ff
rajatgupta <rajat2411gupta@gmail.com>
2021-02-06 00:02:26 +0530

    styling the homepage

--- src/components/Story/Story.jsx
+++ src/components/Story/Story.jsx
@@ -1,18 +1,19 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react';
 import { getStories } from '../../service/Api';
+import styles from "./Story.module.css";
 
 export default function Story({id}) {
 
 	const [story, setStory] = useState({})
 
 	useEffect(() => {
-		getStories(id).then(({data}) => setStory(data) );
+		getStories(id).then(({data}) => data.url && setStory(data) );
 	}, [])
 
 	return (
-		<>
-			<p>{JSON.stringify(story.title)}</p>
-			<a href={story.url} target="_blank">link</a>
-		</>
+		<div className={styles.story}>
+			<a className={styles.title} href={story.url} target="_blank)">{story.title}</a>
+			<p>{story.time}</p>
+		</div>
 	);
 }
